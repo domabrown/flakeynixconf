23a819cfa9ea64521eb62e828dd9dac946da6b63
domabrown <dom-doi@live.co.uk>
2022-08-02 15:33:07 +0100

    remove daedalus

--- flake.nix
+++ flake.nix
@@ -6,17 +6,15 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    daedalus.url = "git+https://gitlab.homotopic.tech/nix/daedalus-flake";
   };
 
-  outputs = { home-manager, nixpkgs, flake-utils,  daedalus, ... }: {
+  outputs = { home-manager, nixpkgs, flake-utils, ... }: {
     nixosConfigurations = {
       hyrax = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-	  daedalus.nixosModules.x86_64-linux.daedalus
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
--- home.nix
+++ home.nix
@@ -9,6 +9,97 @@
   home.username = "dom";
   home.homeDirectory = "/home/dom/";
 
+  # Sway config start
+  home.packages = with pkgs; [
+    swaylock
+    swayidle
+    j4-dmenu-desktop
+    foot
+    waybar
+    font-awesome
+    wl-clipboard
+    mako # notification daemon
+    wofi
+  ];
+   fonts.fontconfig.enable = true;
+  # Use sway desktop environment with Wayland display server
+  wayland.windowManager.sway = {
+   enable = true;
+   wrapperFeatures.gtk = true;
+   # Sway-specific Configuration
+   config = {
+     modifier = "Mod4";
+     terminal = "foot";
+     menu = "j4-dmenu-desktop --no-generic --dmenu='wofi --fork --show drun --allow-images --no-actions' --term=foot";
+     # Status bar(s)
+     bars = [{
+	fonts.size = 15.0;
+	command = "waybar";
+	position = "top";
+      }];
+      input = {
+        "type:keyboard" = {
+	  xkb_layout = "gb";
+	};
+      };
+     # Brightness keys
+      # Display device configuration
+      output = {
+	eDP-1 = {
+	  # Set HIDP scale (pixel integer scaling)
+	  scale = "1";
+	    };
+	  };
+      };
+      # End of Sway-specific Configuration
+    };
+
+    programs.waybar = {
+      enable = true;
+    };
+      
+    # Rust-based terminal emulator
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        env.TERM = "alacritty";
+        window = {
+          decorations = "full";
+          title = "Alacritty";
+          dynamic_title = true;
+          class = {
+            instance = "Alacritty";
+            general = "Alacritty";
+          };
+        };
+        font = {
+          normal = {
+            family = "monospace";
+            style = "regular";
+          };
+          bold = {
+            family = "monospace";
+            style = "regular";
+          };
+          italic = {
+            family = "monospace";
+            style = "regular";
+          };
+          bold_italic = {
+            family = "monospace";
+            style = "regular";
+          };
+          size = 14.00;
+        };
+        colors = {
+          primary = {
+            background = "#1d1f21";
+            foreground = "#c5c8c6";
+          };
+        };
+      };
+    };
+
   # Git
    programs.git = {
     enable = true;
@@ -16,7 +107,7 @@
     delta.enable = true;
 
     userEmail = "dom-doi@live.co.uk";
-    userName = "Dom Brown";
+    userName = "domabrown";
     };
 
   # Fish
@@ -41,5 +132,5 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.05";
+  home.stateVersion = "21.11";
 }
